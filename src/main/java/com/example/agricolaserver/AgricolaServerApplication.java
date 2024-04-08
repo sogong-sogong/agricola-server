e6c5cf29021fcf9d2b9e1529d4bc351838b1b6a6
오유정 <ohyujeong@oyujeong-ui-MacBookPro.local>
2024-04-08 15:57:59 +0900

    first commit

---
+++ src/main/java/com/example/agricolaserver/AgricolaServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.agricolaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgricolaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgricolaServerApplication.class, args);
+    }
+
+}
