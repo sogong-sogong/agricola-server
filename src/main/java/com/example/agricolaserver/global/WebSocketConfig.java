f8b534652a57f4e4237dbdb0d359a6abd54ed6d6
오유정 <ohyujeong@oyujeong-ui-MacBookPro.local>
2024-04-20 17:43:43 +0900

    withsocketjs 코드 삭제

--- src/main/java/com/example/agricolaserver/global/WebSocketConfig.java
+++ src/main/java/com/example/agricolaserver/global/WebSocketConfig.java
@@ -11,14 +11,15 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
-    public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.setApplicationDestinationPrefixes("/send");       //클라이언트에서 보낸 메세지를 받을 prefix
-//        registry.enableSimpleBroker("/room");    //해당 주소를 구독하고 있는 클라이언트들에게 메세지 전달
-    }
-
-    @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws-stomp")   //SockJS 연결 주소
-                .withSockJS(); //버전 낮은 브라우저에서도 적용 가능
+                .setAllowedOriginPatterns("*"); // 허용하는 도메인 주소 (일단 모두 허용으로 바꾸었다.)
+    }
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        // 메세지 구독 요청 url -> 메세지 받을 때
+        registry.enableSimpleBroker("/sub");
+        // 메세지 발행 요청 url -> 메세지 보낼 때
+        registry.setApplicationDestinationPrefixes("/pub");
     }
 }
