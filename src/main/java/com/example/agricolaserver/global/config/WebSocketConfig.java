1f03e271b9a8195c37a031eb20805eebb029bac0
오유정 <yujeong0720@gmail.com>
2024-05-15 02:09:16 +0900

    rabbitmq 테스트

--- src/main/java/com/example/agricolaserver/global/config/WebSocketConfig.java
+++ src/main/java/com/example/agricolaserver/global/config/WebSocketConfig.java
@@ -1,7 +1,9 @@
 package com.example.agricolaserver.global.config;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.util.AntPathMatcher;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -10,6 +12,14 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @RequiredArgsConstructor
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    @Value("${spring.rabbitmq.host}")
+    private String host;
+
+    @Value("${spring.rabbitmq.username}")
+    private String username;
+
+    @Value("${spring.rabbitmq.password}")
+    private String password;
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws-stomp")   //SockJS 연결 주소
@@ -18,9 +28,14 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     }
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        //구독 url : 메시지 받을 때
-        registry.enableSimpleBroker("/sub");
-        // send url : 메시지 보낼 때
+        registry.setPathMatcher(new AntPathMatcher(".")); // URL을 / -> .으로
         registry.setApplicationDestinationPrefixes("/pub");
+        registry.enableStompBrokerRelay("/queue","/topic","/exchange","/amq/queue")
+                .setRelayHost(host)
+                .setRelayPort(61613)
+                .setSystemLogin(username)
+                .setSystemPasscode(password)
+                .setClientLogin(username)
+                .setClientPasscode(password);
     }
 }
