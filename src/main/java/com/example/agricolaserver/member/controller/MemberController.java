25b5521f574bac63c6ef8c9c508aced787a98d38
snavellib <snavellib@naver.com>
2024-05-20 13:12:09 +0900

    card-counts
    card-counts

--- src/main/java/com/example/agricolaserver/member/controller/MemberController.java
+++ src/main/java/com/example/agricolaserver/member/controller/MemberController.java
@@ -1,10 +1,13 @@
 package com.example.agricolaserver.member.controller;
+
 import com.example.agricolaserver.member.dto.CreateMemberDTO;
+import com.example.agricolaserver.member.dto.CardCountDTO;
 import com.example.agricolaserver.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,8 +17,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/member")
 public class MemberController {
     private final MemberService memberService;
-    @GetMapping(path="create")
-    public ResponseEntity<CreateMemberDTO> createMember(){
+
+    @GetMapping(path = "create")
+    public ResponseEntity<CreateMemberDTO> createMember() {
         return memberService.createMember();
+    }
+
+    @GetMapping(path = "/{memberId}/card-counts")
+    public ResponseEntity<CardCountDTO> getCardCountsByMemberId(@PathVariable Long memberId) {
+        return memberService.getCardCountsByMemberId(memberId);
     }
 }
