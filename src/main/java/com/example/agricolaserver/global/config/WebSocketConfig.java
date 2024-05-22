479808e6fcb89d3c9b60ad07704789f2772e0dcd
오유정 <yujeong0720@gmail.com>
2024-05-22 14:26:37 +0900

    방 입장 테스트용

--- src/main/java/com/example/agricolaserver/global/config/WebSocketConfig.java
+++ src/main/java/com/example/agricolaserver/global/config/WebSocketConfig.java
@@ -1,10 +1,17 @@
 package com.example.agricolaserver.global.config;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.messaging.converter.StringMessageConverter;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.scheduling.concurrent.DefaultManagedTaskScheduler;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.web.socket.client.standard.StandardWebSocketClient;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.messaging.WebSocketStompClient;
 
 @Configuration
 @RequiredArgsConstructor
@@ -22,5 +29,16 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         registry.enableSimpleBroker("/sub");
         // send url : 메시지 보낼 때
         registry.setApplicationDestinationPrefixes("/pub");
+    }
+    @Bean
+    public WebSocketStompClient stompClient() {
+        WebSocketStompClient stompClient = new WebSocketStompClient(new StandardWebSocketClient());
+        stompClient.setMessageConverter(new StringMessageConverter());
+        stompClient.setTaskScheduler(new DefaultManagedTaskScheduler());
+        return stompClient;
+    }
+    @Bean
+    public MappingJackson2HttpMessageConverter jsonConverter() {
+        return new MappingJackson2HttpMessageConverter();
     }
 }
--- src/test/java/com/example/agricolaserver/CreateRoomAndMemberTest.java
+++ src/test/java/com/example/agricolaserver/CreateRoomAndMemberTest.java
@@ -2,10 +2,6 @@ package com.example.agricolaserver;
 
 import com.example.agricolaserver.member.dto.CreateMemberDTO;
 import com.example.agricolaserver.room.dto.CreateRoomDTO;
-import com.example.agricolaserver.room.dto.EntranceRequest;
-import com.example.agricolaserver.room.dto.EntranceResponse;
-import jakarta.transaction.Transactional;
-import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,45 +10,23 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.messaging.converter.MappingJackson2MessageConverter;
-import org.springframework.messaging.simp.stomp.StompSession;
-import org.springframework.messaging.simp.stomp.StompSessionHandlerAdapter;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.web.socket.client.standard.StandardWebSocketClient;
-import org.springframework.web.socket.messaging.WebSocketStompClient;
-import org.springframework.web.socket.sockjs.client.SockJsClient;
 import org.springframework.web.socket.sockjs.client.Transport;
 import org.springframework.web.socket.sockjs.client.WebSocketTransport;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
-import java.util.concurrent.*;
 
-import static java.util.Arrays.asList;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-@ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class RoomEntranceTest {
+public class CreateRoomAndMemberTest {
     @LocalServerPort
     private int port;
     @Autowired
     private TestRestTemplate restTemplate;
-//    protected StompSession stompSession;
-//    private final String socketUrl;
-//    private final WebSocketStompClient websocketClient;
-//    public RoomEntranceTest() {
-//        this.websocketClient = new WebSocketStompClient(new SockJsClient(createTransport()));
-//        this.websocketClient.setMessageConverter(new MappingJackson2MessageConverter());
-//        this.socketUrl = "ws://localhost:";
-//    }
-    private List<Transport> createTransport() {
-        List<Transport> transports = new ArrayList<>(1);
-        transports.add(new WebSocketTransport(new StandardWebSocketClient()));
-        return transports;
-    }
     @DisplayName("멤버 생성 테스트")
     @Test
     void createMember(){
@@ -70,18 +44,6 @@ public class RoomEntranceTest {
         assertThat(Objects.requireNonNull(response.getBody()).roomId()).isEqualTo(1L);
     }
 
-//    @BeforeEach
-//    public void connect() throws ExecutionException, InterruptedException, TimeoutException {
-//        this.stompSession = this.websocketClient
-//                .connect(socketUrl + port + "/ws-stomp", new StompSessionHandlerAdapter() {})
-//                .get(3, TimeUnit.SECONDS);
-//    }
-//    @AfterEach
-//    public void disconnect() {
-//        if (this.stompSession.isConnected()) {
-//            this.stompSession.disconnect();
-//        }
-//    }
 //    private List<Transport> createTransport() {
 //        List<Transport> transports = new ArrayList<>(1);
 //        transports.add(new WebSocketTransport(new StandardWebSocketClient()));
