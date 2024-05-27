765b287c2ac229c5a859a3ad6e2da2cd50d90b07
snavellib <snavellib@naver.com>
2024-05-27 13:52:47 +0900

    스코어 조회
    score check

---
+++ src/main/java/com/example/agricolaserver/score/controller/ScoreController.java
@@ -0,0 +1,25 @@
+package com.example.agricolaserver.score.controller;
+
+import com.example.agricolaserver.score.dto.ScoreDTO;
+import com.example.agricolaserver.score.service.ScoreService;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/score")
+@RequiredArgsConstructor
+public class ScoreController {
+    private final ScoreService scoreService;
+    private static final Logger logger = LoggerFactory.getLogger(ScoreController.class);
+
+    @GetMapping("/member/{memberId}")
+    public ResponseEntity<ScoreDTO> getScoreByMemberId(@PathVariable Long memberId) {
+        logger.info("GET /score/member/{}", memberId);
+        ResponseEntity<ScoreDTO> response = scoreService.getScoreByMemberId(memberId);
+        logger.info("Response: {}", response);
+        return response;
+    }
+}
