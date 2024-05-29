1f161bd0ad680fb8ac346df5da137d5970339262
Moonsooin <anstndls1998@gmail.com>
2024-05-29 15:50:08 +0900

    공동창고 업데이트 기능 추가

--- src/main/java/com/example/agricolaserver/commonstorage/controller/CommonStorageController.java
+++ src/main/java/com/example/agricolaserver/commonstorage/controller/CommonStorageController.java
@@ -1,31 +1,34 @@
 package com.example.agricolaserver.commonstorage.controller;
 
+import com.example.agricolaserver.commonstorage.dto.GetCommonStorageResponse;
+import com.example.agricolaserver.commonstorage.dto.UpdateCommonStorageRequestDTO;
 import com.example.agricolaserver.commonstorage.repository.CommonStorageRepository;
+import com.example.agricolaserver.commonstorage.service.CommonStorageService;
 import com.example.agricolaserver.room.domain.Room;
 import com.example.agricolaserver.room.repository.RoomRepository;
 import com.example.agricolaserver.commonstorage.domain.CommonStorage;
 import com.example.agricolaserver.commonstorage.dto.GetCommonStorageDTO;
-import com.example.agricolaserver.storage.dto.GetStorageDTO;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.Optional;
-
-@RestController
+@Controller
 @RequestMapping("/commonstorage")
 public class CommonStorageController {
     private final CommonStorageRepository commonStorageRepository;
+    private final CommonStorageService commonStorageService;
 
-    public CommonStorageController(CommonStorageRepository commonStorageRepository, RoomRepository roomRepository) {
+    public CommonStorageController(CommonStorageRepository commonStorageRepository, RoomRepository roomRepository, CommonStorageService commonStorageService) {
         this.commonStorageRepository = commonStorageRepository;
+        this.commonStorageService = commonStorageService;
     }
 
     @GetMapping("/{roomID}")
     public ResponseEntity<GetCommonStorageDTO> getStorageByRoomID(@PathVariable Room roomID) {
-
 
         CommonStorage commonstorage = commonStorageRepository.findByRoom(roomID);
 
@@ -33,5 +36,12 @@ public class CommonStorageController {
 
         return ResponseEntity.ok(commonstorageDTO);
 
+    }
+
+    @MessageMapping("/room/common/update")
+    @SendTo("/sub/room/{roomId}")
+    public GetCommonStorageResponse getCommonStorage(@Payload UpdateCommonStorageRequestDTO requestDTO) {
+        Room room = requestDTO.getRoomId();
+        return commonStorageService.getCommonStorage(room, requestDTO);
     }
 }
