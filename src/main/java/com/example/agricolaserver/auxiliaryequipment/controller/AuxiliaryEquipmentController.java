95d4b3dc44d62f3f581f361d025c6152a365e99d
snavellib <snavellib@naver.com>
2024-05-31 20:03:57 +0900

    내 보조설비 카드 조회
    AuxiliaryEquipment

---
+++ src/main/java/com/example/agricolaserver/auxiliaryequipment/controller/AuxiliaryEquipmentController.java
@@ -0,0 +1,35 @@
+package com.example.agricolaserver.auxiliaryequipment.controller;
+
+import com.example.agricolaserver.auxiliaryequipment.service.AuxiliaryEquipmentService;
+import com.example.agricolaserver.auxiliaryequipment.service.AuxiliaryEquipmentService.AuxiliaryEquipmentInfo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/aux")
+@RequiredArgsConstructor
+public class AuxiliaryEquipmentController {
+    private final AuxiliaryEquipmentService auxiliaryEquipmentService;
+
+    @GetMapping("/member/{memberId}")
+    public ResponseEntity<List<AuxiliaryEquipmentInfo>> getAuxiliaryEquipmentsByMemberId(@PathVariable Long memberId) {
+        List<AuxiliaryEquipmentInfo> auxiliaryEquipments = auxiliaryEquipmentService.getAuxiliaryEquipmentsByMemberId(memberId);
+        return ResponseEntity.ok(auxiliaryEquipments);
+    }
+    
+    @GetMapping("/member/{memberId}/aux/{cardNumber}")
+    public ResponseEntity<AuxiliaryEquipmentInfo> getAuxiliaryEquipmentByCardNumber(@PathVariable Long memberId, @PathVariable Integer cardNumber) {
+        List<AuxiliaryEquipmentInfo> auxiliaryEquipments = auxiliaryEquipmentService.getAuxiliaryEquipmentsByMemberId(memberId);
+        if (cardNumber >= 1 && cardNumber <= auxiliaryEquipments.size()) {
+            AuxiliaryEquipmentInfo auxiliaryEquipment = auxiliaryEquipments.get(cardNumber - 1); // cardNumber를 0부터가 아니라 1부터 시작하도록 수정
+            return ResponseEntity.ok(auxiliaryEquipment); // 해당 카드 번호의 보조 장비 정보 반환
+        } else {
+            return ResponseEntity.notFound().build(); // 유효하지 않은 카드 번호면 404 에러 반환
+        }
+    }
+    
+    
+}
--- src/main/java/com/example/agricolaserver/auxiliaryequipment/repository/AuxiliaryEquipmentRepository.java
+++ src/main/java/com/example/agricolaserver/auxiliaryequipment/repository/AuxiliaryEquipmentRepository.java
@@ -4,6 +4,9 @@ import com.example.agricolaserver.auxiliaryequipment.domain.AuxiliaryEquipment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface AuxiliaryEquipmentRepository extends JpaRepository<AuxiliaryEquipment,String> {
+public interface AuxiliaryEquipmentRepository extends JpaRepository<AuxiliaryEquipment, String> {
+    List<AuxiliaryEquipment> findAllByMember_Id(Long memberId);
 }
