28b7c858af734f18bd7f9ee962d779fa6a3f77db
snavellib <snavellib@naver.com>
2024-06-03 03:09:57 +0900

    내 우리 조회+업데이트
    cage

---
+++ src/main/java/com/example/agricolaserver/cage/dto/GetCageResponse.java
@@ -0,0 +1,25 @@
+package com.example.agricolaserver.cage.dto;
+
+import com.example.agricolaserver.cage.domain.Cage;
+import lombok.Getter;
+
+@Getter
+public class GetCageResponse {
+    private Long id;
+    private Integer type;
+    private Integer stock;
+    private Integer xy;
+    private Integer stock_cnt;
+    private Integer field;
+
+    public static GetCageResponse from(Cage cage) {
+        GetCageResponse response = new GetCageResponse();
+        response.id = cage.getId();
+        response.type = cage.getType();
+        response.stock = cage.getStock();
+        response.xy = cage.getXy();
+        response.stock_cnt = cage.getStock_cnt();
+        response.field = cage.getField();
+        return response;
+    }
+}
